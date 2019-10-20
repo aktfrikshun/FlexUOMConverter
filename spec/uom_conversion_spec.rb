d661c8642b2de706525240179856264df1acca14
Allen Taylor <ataylor@calipercorp.com>
2019-10-20 17:53:28 -0400

    Broke tests to show not allow commit

--- spec/uom_conversion_spec.rb
+++ spec/uom_conversion_spec.rb
@@ -27,7 +27,7 @@ describe 'UOMConversion' do
       it "can convert volume" do
         UOMConversion.listUnitsForProperty('volume').each do | unit1 |
           UOMConversion.listUnitsForProperty('volume').each do | unit2 |
-            expect(UOMConversion.check_answer(1,unit1,unit2,1)).not_to eql('indvalid')
+            expect(UOMConversion.check_answer(1,unit1,unit2,1)).not_to eql('invalid')
           end
         end
       end
@@ -35,7 +35,7 @@ describe 'UOMConversion' do
       it "can can't convert different properties" do
         UOMConversion.listUnitsForProperty('volume').each do | unit1 |
           UOMConversion.listUnitsForProperty('temparature').each do | unit2 |
-            expect(UOMConversion.check_answer(1,unit1,unit2,1)).to eql('invalid')
+            expect(UOMConversion.check_answer(1,unit1,unit2,1)).to eql('idfnvalid')
           end
         end
       end
